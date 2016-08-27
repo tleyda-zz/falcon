dc4876bacf1cf3f4ff8083835b55d09cda2daf63
Troy Leyda <tleyda@pillartechnology.com>
2016-08-27 15:31:37 -0500

    Add X to M

--- src/roman_calc.c
+++ src/roman_calc.c
@@ -85,6 +85,10 @@ void roman_calc_add(RomanCalculator* roman_calc, char* romanOperand1, char* roma
 			roman_calc->result[0] = 'D';
 			roman_calc->result[1] = 'X';
 			break;
+		case 'M':
+			roman_calc->result[0] = 'M';
+			roman_calc->result[1] = 'X';
+			break;
 		}
 		break;
 	case 'C':
--- tests/check_roman_calc.c
+++ tests/check_roman_calc.c
@@ -173,6 +173,14 @@ START_TEST(test_roman_calc_add_X_to_D)
 }
 END_TEST
 
+START_TEST(test_roman_calc_add_X_to_M)
+{
+    roman_calc_add(roman_calc, "X", "M");
+    ck_assert_int_eq (roman_calc_result_length(roman_calc), 2);
+    ck_assert_str_eq (roman_calc_result(roman_calc), "MX");
+}
+END_TEST
+
 Suite * roman_calc_suite (void)
 {
     Suite *s = suite_create ("Roman Calculator");
@@ -204,6 +212,7 @@ Suite * roman_calc_suite (void)
     tcase_add_test (tc_add, test_roman_calc_add_X_to_V);
     tcase_add_test (tc_add, test_roman_calc_add_X_to_C);
     tcase_add_test (tc_add, test_roman_calc_add_X_to_D);
+    tcase_add_test (tc_add, test_roman_calc_add_X_to_M);
     suite_add_tcase (s, tc_add);
 
     return s;
