c7bbc3275808fb7cf317a1ae77ad1a8115d419c4
Troy Leyda <tleyda@pillartechnology.com>
2016-08-27 15:09:59 -0500

    Add V to M

--- tests/check_roman_calc.c
+++ tests/check_roman_calc.c
@@ -133,6 +133,14 @@ START_TEST(test_roman_calc_add_V_to_D)
 }
 END_TEST
 
+START_TEST(test_roman_calc_add_V_to_M)
+{
+    roman_calc_add(roman_calc, "V", "M");
+    ck_assert_int_eq (roman_calc_result_length(roman_calc), 2);
+    ck_assert_str_eq (roman_calc_result(roman_calc), "MV");
+}
+END_TEST
+
 Suite * roman_calc_suite (void)
 {
     Suite *s = suite_create ("Roman Calculator");
@@ -159,6 +167,7 @@ Suite * roman_calc_suite (void)
     tcase_add_test (tc_add, test_roman_calc_add_V_to_X);
     tcase_add_test (tc_add, test_roman_calc_add_V_to_C);
     tcase_add_test (tc_add, test_roman_calc_add_V_to_D);
+    tcase_add_test (tc_add, test_roman_calc_add_V_to_M);
     suite_add_tcase (s, tc_add);
 
     return s;
