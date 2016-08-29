845c322b5ce600354e0c35c29017b91a5acc21c9
Troy Leyda <tleyda@pillartechnology.com>
2016-08-29 17:14:55 -0500

    Add C to X

--- src/roman_calc.c
+++ src/roman_calc.c
@@ -110,6 +110,10 @@ void roman_calc_add(RomanCalculator* roman_calc, char* romanOperand1, char* roma
 			roman_calc->result[0] = 'C';	
 			roman_calc->result[1] = 'V';
 			break;
+		case 'X':
+			roman_calc->result[0] = 'C';	
+			roman_calc->result[1] = 'X';
+			break;
 		case 'C':
 			roman_calc->result[0] = 'C';	
 			roman_calc->result[1] = 'C';
