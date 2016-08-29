19fc3558eac134f15f882653dc2f3e5576756518
Troy Leyda <tleyda@pillartechnology.com>
2016-08-29 17:23:21 -0500

    refactor adding C

--- src/roman_calc.c
+++ src/roman_calc.c
@@ -103,32 +103,17 @@ void roman_calc_add(RomanCalculator* roman_calc, char* romanOperand1, char* roma
 		switch(romanOperand2[0])
 		{
 		case 'I':
-			roman_calc->result[0] = 'C';	
-			roman_calc->result[1] = 'I';
-			break;
 		case 'V':
-			roman_calc->result[0] = 'C';	
-			roman_calc->result[1] = 'V';
-			break;
 		case 'X':
-			roman_calc->result[0] = 'C';	
-			roman_calc->result[1] = 'X';
-			break;
 		case 'L':
-			roman_calc->result[0] = 'C';	
-			roman_calc->result[1] = 'L';
-			break;
 		case 'C':
-			roman_calc->result[0] = 'C';	
-			roman_calc->result[1] = 'C';
+			roman_calc->result[0] = romanOperand1[0];	
+			roman_calc->result[1] = romanOperand2[0];
 			break;
 		case 'D':
-			roman_calc->result[0] = 'D';	
-			roman_calc->result[1] = 'C';
-			break;
 		case 'M':
-			roman_calc->result[0] = 'M';	
-			roman_calc->result[1] = 'C';
+			roman_calc->result[0] = romanOperand2[0];	
+			roman_calc->result[1] = romanOperand1[0];
 			break;
 		}
 		break;
