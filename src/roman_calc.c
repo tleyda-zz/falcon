4ac4cba0b989ce3da1f91c3254ab1c3da2f3f348
Troy Leyda <tleyda@pillartechnology.com>
2016-08-29 16:55:05 -0500

    Add L to M

--- src/roman_calc.c
+++ src/roman_calc.c
@@ -102,6 +102,10 @@ void roman_calc_add(RomanCalculator* roman_calc, char* romanOperand1, char* roma
 			roman_calc->result[0] = 'D';
 			roman_calc->result[1] = 'L';
 			break;
+		case 'M':
+			roman_calc->result[0] = 'M';
+			roman_calc->result[1] = 'L';
+			break;
 		}
 		break;
 	case 'C':
