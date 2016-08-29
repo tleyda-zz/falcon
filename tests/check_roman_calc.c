3252491f33bd6841e54e586f8a5fa6c406020018
Troy Leyda <tleyda@pillartechnology.com>
2016-08-29 16:53:46 -0500

    Add L to D

--- tests/check_roman_calc.c
+++ tests/check_roman_calc.c
@@ -221,6 +221,14 @@ START_TEST(test_roman_calc_add_L_to_D)
 }
 END_TEST
 
+START_TEST(test_roman_calc_add_L_to_M)
+{
+    roman_calc_add(roman_calc, "L", "M");
+    ck_assert_int_eq (roman_calc_result_length(roman_calc), 2);
+    ck_assert_str_eq (roman_calc_result(roman_calc), "ML");
+}
+END_TEST
+
 Suite * roman_calc_suite (void)
 {
     Suite *s = suite_create ("Roman Calculator");
@@ -258,6 +266,7 @@ Suite * roman_calc_suite (void)
     tcase_add_test (tc_add, test_roman_calc_add_L_to_X);
     tcase_add_test (tc_add, test_roman_calc_add_L_to_C);
     tcase_add_test (tc_add, test_roman_calc_add_L_to_D);
+    tcase_add_test (tc_add, test_roman_calc_add_L_to_M);
     suite_add_tcase (s, tc_add);
 
     return s;
